dba6697e97a8c7bae38bbb3d071874fa900503a5
nikitos23133 <151367384+nikitos23133@users.noreply.github.com>
2025-04-10 21:12:22 +0300

    waf.tf

---
+++ waf.tf
@@ -0,0 +1,26 @@
+# security.tf
+resource "aws_wafv2_web_acl" "main" {
+  name  = "codemaster-waf"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 1
+    override_action { none {} }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled  = true
+    }
+  }
+}
